1a1f3ab9efe68d5522ae560195d9db55ea486501
Paul <23568795+sktaylortrash@users.noreply.github.com>
2019-02-20 02:12:01 -0600

    Readme Update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-"# atTiny85-Watchdog" 
+This sketch is for a DigiSpark Tiny85 or AdaFruit Trinket
+
+Acts as a hardware watchdog on any host that has a reset pin which functions by being pulled LOW to ground.
+
+Monitors hbPin, requiring a state change within hbTime.
+
+Onboard LED mimics the state of the hbPin signal from the host, giving a visual heartbeat of both hosts with one LED.
+
+If a state change is not detected, resetPin is pulled LOW to force a reset on the host.
+ResetPin on the watchdog needs to be connected to the RST pin on the host and both devices need to be on a common ground.
+
+On startup and after a reset, resetTime is invoked to insure the host has time to properly start up and start emitting the heartbeat signal.
--- atTiny85WatchDog.ino
+++ atTiny85WatchDog.ino
@@ -1,20 +1,3 @@
-// This sketch is for a DigiSpark atTiny85
-//
-// The Trinket acts as a hardware watchdog on any host that has a reset pin which
-// functions by being pulled LOW to ground.
-//
-// The Trinket monitors hbPin, requiring a state change within hbTime.
-//
-// The Trinket LED mimics the state of the hbPin signal from the host, giving a visual
-// heartbeat of both hosts with one LED.
-//
-// If a state change is not detected, resetPin is pulled LOW to force a reset on the host.
-// Obviously, resetPin on the Trinket needs to be connected to the RST pin on the host and both
-// devices need to be on a common ground.
-//
-// On startup and after a reset, resetTime is invoked to insure the host has time to
-// properly start up and start emitting the heartbeat signal.
-
 #include "timer.h"
 
 // constants won't change -- used to set pin numbers and define delays
