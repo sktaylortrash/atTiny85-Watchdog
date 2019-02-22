c89c3f30492802c8a777b9f3b971652b220a388f
Paul <23568795+sktaylortrash@users.noreply.github.com>
2019-02-22 05:22:45 -0600

    Update atTiny85WatchDog.ino

--- atTiny85WatchDog.ino
+++ atTiny85WatchDog.ino
@@ -1,13 +1,10 @@
 #include <SimpleTimer.h>
 
-// constants won't change -- used to set pin numbers and define delays
-const int hbPin = 4; // Use the signal from the host to set the heartbeat status
-const int resetPin = 3; // use this pin to reset the host
-const int ledPin = 1; // built-in LED on atTiny85
-int wd_timer_id;
+const int wdPin = 4;
+const int resetPin = 3;
+const int ledPin = 1;
 
-// global variables
-int count = 0;
+int wd_timer_id;
 int currState = HIGH;
 int prevState = LOW;
 
@@ -15,51 +12,30 @@ int prevState = LOW;
 SimpleTimer timer;
 
 void wdCallback() {
-resetHost(resetPin); 
+  resetHost(resetPin); 
 }
 
-
-/* --------------------------------------------------------------------------------- */
-
 void setup() {
-  // Set pin modes
   pinMode(resetPin, OUTPUT);
-  // Make sure we are turned on to start!
   digitalWrite(resetPin, HIGH);
   wd_timer_id = timer.setInterval(30000, wdCallback);
 }
 
-/* --------------------------------------------------------------------------- */
-
 void loop() {
-timer.run();
-ledHeartbeat();
+  timer.run();
+  ledHeartbeat();
 }
-
-/* --------------------------------------------------------------------------------- */
 
 void ledHeartbeat(void) {
-
-// Pulse the LED to show WE are alive!
-// Use the host's heartbeat signal to drive the LED
-// This way we have visibility on both controllers with 1 indicator
-
-digitalWrite(ledPin, currState = digitalRead(hbPin));
-if ( currState != prevState) { // State has changed
-prevState = currState; // Update
-timer.restartTimer(wd_timer_id);
+  digitalWrite(ledPin, currState = digitalRead(wdPin));
+  if ( currState != prevState) { // State has changed
+    prevState = currState; // Update
+    timer.restartTimer(wd_timer_id);
+  }
 }
-
-
-}
-
-/* -------------------------------------------------------------------------------- */
 
 void resetHost(int resetPin) {
-
-digitalWrite(resetPin, LOW); // Turn the power relay (transistor) off
-delay(5000); // 5 seconds so we don't cycle any bulbs too fast
-digitalWrite(resetPin, HIGH); // Turn it back on so the host can boot!
-
-
+  digitalWrite(resetPin, LOW); 
+  delay(5000); 
+  digitalWrite(resetPin, HIGH);
 }
